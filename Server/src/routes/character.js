59d4c079b3b208565227d461a79ee072bfdffb46
MiguelSc66 <miguel.scaccia1@gmail.com>
2023-07-21 21:44:03 -0300

    async await

--- Server/src/routes/character.js
+++ Server/src/routes/character.js
@@ -3,6 +3,6 @@ const express = require("express");
 const { getCharById } = require("../controllers/getCharById");
 const characterRouter = express.Router();
 
-characterRouter.get("/:id", getCharById)
+characterRouter.get("/:idChar", getCharById)
 
 module.exports = characterRouter
